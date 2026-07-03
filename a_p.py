68d532fe175c32e9d513fd29374da025e12619f2
shravanpradeep1234-cloud <shravanpradeep1234@gmail.com>
2026-07-03 14:27:52 +0530

    Update a_p.py

--- a_p.py
+++ a_p.py
@@ -99,7 +99,7 @@ st.set_page_config(page_title="PragyanAI Video Creator", layout="wide")
 if os.path.exists("thor-png-18501.png"):
     st.image("thor-png-18501.png")
 
-st.title("PragyanAI - Multimedia Merger")
+st.title("XRAVAN - Multimedia Merger")
 st.markdown("Upload multiple images, specify timing, and add audio from a file or YouTube.")
 
 with st.sidebar:
